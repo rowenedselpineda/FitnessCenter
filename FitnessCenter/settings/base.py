0de6d02d05ce480aa9751bdfcb322547c58417ec
Rowen_Pineda <rdpineda1@student.hau.edu.ph>
2023-11-27 02:57:27 +0800

    HI

--- FitnessCenter/settings/base.py
+++ FitnessCenter/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -114,7 +115,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
@@ -122,3 +122,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
--- authenticator/urls.py
+++ authenticator/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path, include
+from django.urls import path
 from authenticator import views
 
 urlpatterns = [
-    path('', views.home, name='Home'),
+    path('', views.home, name="Home"),
+    path('signup', views.signup, name="signup"),
+    path('login', views.handleLogin, name="handlelogin"),
+    path('logout', views.handleLogout, name="handleLogout"),
+    path('contact', views.contact, name="contact"),
+    path('join', views.enroll, name="enroll"),
+    path('profile', views.profile, name="profile"),
+    path('gallery', views.gallery, name="gallery"),
+    path('attendance', views.attendance, name="attendance"),
 
 ]
